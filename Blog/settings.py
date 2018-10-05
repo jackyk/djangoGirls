953bc5a0ac34cb4e0185dbcf76fe04a530602747
jackyk <jackykimani13@gmail.com>
2018-10-05 00:21:25 +0000

    setting up database and timezone

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rhvbiz(4coia!0pye5n3**_p)i@t%($tep203#e06y65b4$05-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
