d386fcbb93ac0e68170e508a3ce1f84147f930cd
jackyk <jackykimani13@gmail.com>
2018-10-05 00:42:43 +0000

    django admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Post
+# make our post visible -- login credentials
+# create super user account
+admin.site.register(Post)
